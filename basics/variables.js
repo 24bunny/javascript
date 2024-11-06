578a6bbef1762947e8b9b2231da461a8b60f090e
24bunny <156573680+24bunny@users.noreply.github.com>
2024-11-06 11:28:11 +0000

    commit

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=1443
+let accountEmail="kanishk@dsd.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+
+accountEmail="sddasd@sdsd.com"
+accountPassword="2432"
+accountCity="pune"
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
